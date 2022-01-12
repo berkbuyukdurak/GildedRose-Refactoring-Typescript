c89af4665e5e6c178bbf1a7202c6192874954c67
C:\Users\berkb\Desktop\Workspaces\SourceTree Workspace <berkbuyukdurak07@gmail.com>
2022-01-12 16:45:33 +0300

    Test class is updated.

--- test/golden-master-text-test.ts
+++ test/golden-master-text-test.ts
@@ -1,4 +1,5 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { GildedRose } from '@/gilded-rose';
+import { Item } from '@/models/item';
 
 const items = [
   new Item("+5 Dexterity Vest", 10, 20), //
