a72f8562f612cab1bd7ac1ec42b437242f8c8adf
C:\Users\berkb\Desktop\Workspaces\SourceTree Workspace <berkbuyukdurak07@gmail.com>
2022-01-14 13:46:27 +0300

    Index is added.

--- app/inventory/index.ts
+++ app/inventory/index.ts
@@ -2,4 +2,5 @@ export {Item} from '@/models/item';
 export {default as General} from './general';
 export {default as Aged} from './aged-brie';
 export {default as Conjured} from './conjured';
+export {default as Legendary} from './legendary';
 export {default as BackstageTicket} from './backstage-ticket';
