5e2adee78e1ebc4ee8e822a85e63ea1970ca37a2
C:\Users\berkb\Desktop\Workspaces\SourceTree Workspace <berkbuyukdurak07@gmail.com>
2022-01-14 14:59:34 +0300

    Tests are completed.

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -1,9 +1,67 @@
-import {GildedRose } from '@/gilded-rose';
-import {Item} from '@/models/item';
+import {expect} from 'chai';
+import { GildedRose } from '@/gilded-rose';
+import { Aged, BackstageTicket, Conjured, General, Item, Legendary } from '@/inventory';
+
+
 describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('Backstage passes to a TAFKAL80ETC concert');
+
+  /**
+   * Test gilded rose constructor
+   */
+  it('should be empty', () => {
+      const gildedRose = new GildedRose();
+      expect(gildedRose.items).to.be.empty;
   });
+
+  /**
+   * Test gilded rose inventory categorization
+   */
+  it('should contain the correct object instances', () => {
+      const items = [
+          new Item("Aged Brie", 2, 0),
+          new Item("Elixir of the Mongoose", 5, 7),
+          new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+          new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+          new Item("Conjured Mana Cake", 3, 6)
+      ];
+
+      const gildedRose = new GildedRose(items);
+      expect(gildedRose.items[0]).to.be.an.instanceof(Aged);
+      expect(gildedRose.items[1]).to.be.an.instanceof(General);
+      expect(gildedRose.items[2]).to.be.an.instanceof(Legendary);
+      expect(gildedRose.items[3]).to.be.an.instanceof(BackstageTicket);
+      expect(gildedRose.items[4]).to.be.an.instanceof(Conjured);
+  });
+
+  /**
+   * Test gilded rose update quality function
+   */
+  it('should update quality + sellIn values correctly', () => {
+      const items = [
+          new Item("Aged Brie", 2, 0),
+          new Item("Elixir of the Mongoose", 5, 7),
+          new Item("Sulfuras, Hand of Ragnaros", 10, 55),
+          new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+          new Item("Conjured Mana Cake", 3, 6)
+      ];
+
+      const gildedRose = new GildedRose(items);
+      gildedRose.updateQuality();
+
+      expect(gildedRose.items[0].sellIn).to.equal(1);
+      expect(gildedRose.items[0].quality).to.equal(1);
+
+      expect(gildedRose.items[1].sellIn).to.equal(4);
+      expect(gildedRose.items[1].quality).to.equal(6);
+
+      expect(gildedRose.items[2].sellIn).to.equal(0);
+      expect(gildedRose.items[2].quality).to.equal(80);
+
+      expect(gildedRose.items[3].sellIn).to.equal(14);
+      expect(gildedRose.items[3].quality).to.equal(21);
+
+      expect(gildedRose.items[4].sellIn).to.equal(2);
+      expect(gildedRose.items[4].quality).to.equal(4);
+  });
+
 });
---
+++ test/jest/legendary.spec.ts
@@ -0,0 +1,29 @@
+import {expect} from 'chai';
+import { Legendary } from '@/inventory';
+
+
+describe('Legendary Inventory', () => {
+
+    /**
+     * Test constructor
+     */
+    it('should abide by the constructor limit', () => {
+        const ragnaros = new Legendary('Sulfuras, Hand of Ragnaros', 80, -1);
+        expect(ragnaros.sellIn).to.equal(0);
+        expect(ragnaros.quality).to.equal(80);
+
+        const helios = new Legendary('Sulfuras, Hand of Helios', 80, 100);
+        expect(helios.sellIn).to.equal(0);
+        expect(helios.quality).to.equal(80);
+    });
+
+    /**
+     * Test degradation
+     */
+    it('should NOT degrade', () => {
+        const ragnaros = new Legendary('Sulfuras, Hand of Ragnaros', 100, 10);
+        ragnaros.updateQuality();
+        expect(ragnaros.sellIn).to.equal(0);
+        expect(ragnaros.quality).to.equal(100);
+    });
+});
